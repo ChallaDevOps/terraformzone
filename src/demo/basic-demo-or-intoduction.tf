298ea3bc494278580d2b2a3a19a9e6a0f6f816cb
“DevOps” <DevOps@gmail.com>
2024-01-05 07:52:48 +0530

    Welcome to Terraform Demo

---
+++ src/demo/basic-demo-or-intoduction.tf
@@ -0,0 +1,32 @@
+
+#Printing the statement
+output "greetings" {
+    value = "Welcome to Terraform Learning zone"
+}
+
+
+
+##run the following commands to execute this
+# terraform init
+# terraform apply  
+##provide yes and notice the output
+##op
+
+
+# Changes to Outputs:
+#   + greetings = "Welcome to Terraform Learning zone"
+
+# You can apply this plan to save these new output values to the Terraform state, without changing any real infrastructure.
+
+# Do you want to perform these actions?
+#   Terraform will perform the actions described above.
+#   Only 'yes' will be accepted to approve.
+
+#   Enter a value: yes
+
+
+# Apply complete! Resources: 0 added, 0 changed, 0 destroyed.
+
+# Outputs:
+
+# greetings = "Welcome to Terraform Learning zone"
